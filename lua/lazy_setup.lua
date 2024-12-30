e1f6b77af52ec2dd2e0b5b8381a4481e141993a9
Julio Rafael <juliorafaelnft@hotmail.com>
2024-12-30 18:54:10 -0300

    change theme

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,25 +1,35 @@
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
-    version = "^4", -- Remove version tracking to elect for nightly AstroNvim
+    version = "^4",
     import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+    opts = {
+      mapleader = " ",
+      maplocalleader = ",",
+      icons_enabled = true,
+      pin_plugins = nil,
+      update_notifications = true,
     },
   },
   { import = "community" },
   { import = "plugins" },
-} --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup {
+        style = "vulgaris",
+        transparent = false,
+      }
+      require("bamboo").load()
+    end,
+  },
+}, {
   install = { colorscheme = { "astrotheme", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
-      -- disable some rtp plugins, add more to your liking
       disabled_plugins = {
         "gzip",
         "netrwPlugin",
@@ -29,4 +39,4 @@ require("lazy").setup({
       },
     },
   },
-} --[[@as LazyConfig]])
+})
