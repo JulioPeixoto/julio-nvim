c9775e3bc5e97720221ce02242273fe6907206cb
Julio Rafael <juliorafaelnft@hotmail.com>
2024-12-30 17:36:35 -0300

    set config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,11 @@
+
+---@type LazySpec
+return {
+    require("neo-tree").setup({
+        filesystem = {
+            follow_current_file = true,
+            hijack_netrw_behavior = "open_current",
+        },
+    })
+}
+ 
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+    highlight = {
+      enable = true, -- Habilitar realce de sintaxe
+    },
+  }
+}
